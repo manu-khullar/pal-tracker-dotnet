f814e573a64d6e8dd7a8d0318630fece4862513b
manu-khullar <khullar4u@gmail.com>
2018-06-06 10:02:41 -0600

    Complete Health monitoring

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -48,6 +48,8 @@ namespace PalTracker
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
             services.AddCloudFoundryActuators(Configuration);
             services.AddSingleton<IHealthContributor, TimeEntryHealthContributor>();
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
